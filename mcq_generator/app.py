f16df0f9920b9ea1ea2b77f42fc0eeb3ec4738c8
Aleksey Eliseev <alekseyeliseev@ya.ru>
2024-10-02 00:20:53 +0300

    Добавляет источник к прогнозу

--- mcq_generator/app.py
+++ mcq_generator/app.py
@@ -11,7 +11,10 @@ from langchain.chains.combine_documents.stuff import StuffDocumentsChain
 from langchain.chains.llm import LLMChain
 import string
 import re
+import json
 import docx
+import pandas as pd
+import io
 
 def postprocess_text(text):
     printable = (
@@ -27,11 +30,22 @@ def postprocess_text(text):
     cleaned_text = re.sub('[\n-\x0c]', '', cleaned_text)
     return cleaned_text
 
+def result_to_csv(text):
+    cleaned_prediction = re.sub(r'```json|```', '', text).strip()
+    json_objects = re.findall(r'\{.*?\}', cleaned_prediction, re.DOTALL)
+    parsed_data = [json.loads(obj) for obj in json_objects]
+    df = pd.DataFrame(parsed_data)
+    
+    # Сохранение в буфер
+    output = io.BytesIO()
+    df.to_excel(output, index=False)
+    output.seek(0)  # Перемещение указателя в начало буфера
+    return output
+
 st.title("Упроститель текста и генератор вопросов")
 uploaded_file = st.file_uploader("Загрузите DOCX файл", type=["docx"])
 
 if uploaded_file is not None:
-    # Use a library like python-docx to extract text from DOCX files
     def extract_text_from_docx(file):
         doc = docx.Document(file)
         return '\n'.join([para.text for para in doc.paragraphs])
@@ -71,7 +85,7 @@ if uploaded_file is not None:
         llm_chain = LLMChain(llm=chat_model, prompt=prompt)
         stuff_chain = StuffDocumentsChain(llm_chain=llm_chain, document_variable_name="text")
         selected_chunk_summary = stuff_chain.run(doc)
-        st.write(selected_chunk_summary)
+        st.markdown(selected_chunk_summary)
 
     if st.session_state.generate_questions:
         processed_text = selected_chunk
@@ -80,7 +94,9 @@ if uploaded_file is not None:
             ResponseSchema(name="option_1", description="Первый вариант ответа на вопрос с множественным выбором. Используйте этот формат: 'a) вариант ответа'"),
             ResponseSchema(name="option_2", description="Второй вариант ответа на вопрос с множественным выбором. Используйте этот формат: 'b) вариант ответа''"),
             ResponseSchema(name="option_3", description="Третий вариант ответа на вопрос с множественным выбором. Используйте этот формат: 'c) вариант ответа''"),
-            ResponseSchema(name="answer", description="Правильный ответ на вопрос. Используйте этот формат: 'd) вариант ответа'' или 'b) вариант ответа'', и так далее.")
+            ResponseSchema(name="answer", description="Правильный ответ на вопрос. Используйте этот формат: 'd) вариант ответа'' или 'b) вариант ответа'', и так далее."),
+            ResponseSchema(name="topic_number", description="Номер пункта исходного документа."),
+            ResponseSchema(name="topic", description="Текст пункта исходного документа с номером topic_number"),
         ]
         output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
         format_instructions = output_parser.get_format_instructions()
@@ -95,4 +111,14 @@ if uploaded_file is not None:
         user_query = prompt.format_prompt(user_prompt=processed_text)
         user_query_output = chat_model(user_query.to_messages())
         st.write("Сгенерированные вопросы:")
-        st.write(user_query_output.content)
+        result = user_query_output.content
+        st.write(result)
+        
+        # Получение буфера с данными и добавление кнопки для скачивания
+        output_buffer = result_to_csv(result)
+        st.download_button(
+            label="Скачать файл output.xlsx",
+            data=output_buffer,
+            file_name="output.xlsx",
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
